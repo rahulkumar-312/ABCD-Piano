6738475b1f039779b265d84a4536543074990f15
rahulkumar-312 <125283602+rahulkumar-312@users.noreply.github.com>
2023-06-25 22:34:31 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,36 @@
+let audioElement = new Audio("music/a.wav");
+let btnA = document.getElementById("a");
+
+btnA.addEventListener("click", ()=>{
+    audioElement.play();
+});
+
+/// B Button
+
+let audioElementB = new Audio("music/b.mp3");
+let btnB = document.getElementById("b");
+
+btnB.addEventListener("click", ()=>{
+    audioElementB.play();
+});
+
+
+// C Button
+
+let audioElementC = new Audio("music/c.wav");
+let btnC = document.getElementById("c");
+
+btnC.addEventListener("click", ()=>{
+    audioElementC.play();
+});
+
+// D button
+
+let audioElementD = new Audio("music/d.wav");
+let btnD = document.getElementById("d");
+
+btnD.addEventListener("click", ()=>{
+    audioElementD.play();
+});
+
+
